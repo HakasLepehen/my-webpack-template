886f2cd6d0393fb30abf898cde5cb2b570f566f1
Pavel Petrov <doctorrosen2016@gmail.com>
2021-07-24 22:09:22 +0600

    completed project

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,4 @@
 import '../scss/main.scss';
 import '../assets/favicon.ico';
-import '../img/avatar.jpg';
-import '../fonts/ZenLoop-Regular.ttf';
 
 console.log('Application was started');
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-
 const filename = (ext) =>
   isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 
@@ -12,16 +11,23 @@ const isProd = !isDev;
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
+
   mode: "development",
+
   entry: "./js/main.js",
+
   output: {
     filename: `./js/${filename("js")}`,
     path: path.resolve(__dirname, "app"),
     publicPath: "",
+    assetModuleFilename: `[path]${filename('[ext]')}`
   },
+
+  devtool: "inline-source-map",
 
   devServer: {
     contentBase: path.resolve(__dirname, "app"),
+    // hot: true,
     port: 4200,
   },
 
